082a56f73b84f024c17e6306593700c94217e110
sayache-nova <salim.ayache@nova-technology.fr>
2022-12-09 00:15:37 +0100

    init project

--- apps/nest-api-lib/src/app/app.service.ts
+++ apps/nest-api-lib/src/app/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to nest-api-lib!' };
+  getHelloMessage(): { message: string } {
+    return { message: 'Hello world!' };
   }
 }
