a37715e3f57a882606f62551b9fdf609580a3dd1
sayache-nova <salim.ayache@nova-technology.fr>
2022-12-10 00:44:19 +0100

    added logs

--- apps/nest-api-lib/src/app/controllers/car.controller.ts
+++ apps/nest-api-lib/src/app/controllers/car.controller.ts
@@ -1,13 +1,22 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+import { Request } from 'express';
 
 import { CarService } from '../services/car.service';
 
-@Controller()
+@ApiTags('cars')
+@Controller('cars')
 export class CarController {
   constructor(private readonly carService: CarService) {}
 
   @Get()
-  getCars() {
+  getCars(@Req() request: Request) {
+    Logger.log(`
+      The cars controller has received a GET request, 
+      Here's the request url ${request.url}
+      `);
+
     return this.carService.getCars();
   }
 
--- apps/nest-api-lib/src/main.ts
+++ apps/nest-api-lib/src/main.ts
@@ -1,17 +1,28 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //setting a global prefix
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  //swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Nest Api')
+    .setDescription('The Nest API description')
+    .setVersion('1.0')
+    .addTag('cars')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  //start the server
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
